30ecf351d5eeed6e23978f08a74118cccff4064c
Fabs <31155026+Cool-Dude-Fab@users.noreply.github.com>
2024-04-27 21:52:23 +0100

    Update c4440001.lua

--- script/T.G. Support/c4440001.lua
+++ script/T.G. Support/c4440001.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function c4440001.initial_effect(c)
     -- Synchro Summon
     c:EnableReviveLimit()
-    aux.AddSynchroProcedure(c,nil,1,1,aux.NonTuner(nil),1,99)
+    aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsType,TYPE_TUNER),1,1,aux.FilterBoolFunction(Card.IsType,TYPE_SYNCHRO),1,99)
     
     -- Destroy target when Special Summoned
     local e1=Effect.CreateEffect(c)
@@ -18,7 +18,7 @@ function c4440001.initial_effect(c)
     e1:SetCountLimit(1,4440001)
     c:RegisterEffect(e1)
 
-    -- Protect "T.G." Synchro Monsters
+    -- Protection via banishment
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(4440001,1))
     e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
@@ -29,7 +29,7 @@ function c4440001.initial_effect(c)
     e2:SetOperation(c4440001.repop)
     c:RegisterEffect(e2)
 
-    -- Set "T.G." Spell/Trap
+    -- Set "T.G." Spell/Trap when banished
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(4440001,2))
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -42,12 +42,12 @@ function c4440001.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
--- Destroy target condition
+-- Special Summon Condition
 function c4440001.descon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 
--- Destroy target target
+-- Destroy Target
 function c4440001.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) end
     if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
@@ -56,7 +56,6 @@ function c4440001.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 
--- Destroy target operation
 function c4440001.desop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc and tc:IsRelateToEffect(e) then
@@ -64,9 +63,9 @@ function c4440001.desop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Replacement effect for "T.G." Synchro Monsters
+-- Protection Replace
 function c4440001.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return not e:GetHandler():IsStatus(STATUS_DESTROY_CONFIRMED)
+    if chk==0 then return e:GetHandler():IsReason(REASON_EFFECT)
         and Duel.IsExistingMatchingCard(c4440001.repfilter,tp,LOCATION_GRAVE,0,1,nil) end
     return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
 end
@@ -87,24 +86,24 @@ function c4440001.repop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Set "T.G." Spell/Trap condition
+-- Set Spell/Trap Condition
 function c4440001.setcond(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsFaceup()
 end
 
--- Set "T.G." Spell/Trap target
+-- Set Spell/Trap Target
 function c4440001.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(c4440001.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingCard(c4440001.stfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 end
 
-function c4440001.filter(c)
+function c4440001.stfilter(c)
     return c:IsSetCard(0x27) and (c:IsType(TYPE_SPELL) or c:IsType(TYPE_TRAP)) and c:IsSSetable()
 end
 
--- Set "T.G." Spell/Trap operation
+-- Set Spell/Trap Operation
 function c4440001.setop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-    local g=Duel.SelectMatchingCard(tp,c4440001.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,c4440001.stfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
     if g:GetCount()>0 then
         Duel.SSet(tp,g)
         Duel.ConfirmCards(1-tp,g)
