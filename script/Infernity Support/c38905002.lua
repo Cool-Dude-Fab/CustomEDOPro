cf89ddc43e8648c21b86818c555645cc5f6065b9
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-02 00:42:25 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -16,39 +16,83 @@ function s.initial_effect(c)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_SZONE)
     e2:SetCountLimit(1, id)
+    e2:SetCondition(s.spcon)
     e2:SetCost(s.spcost)
     e2:SetTarget(s.sptarget)
     e2:SetOperation(s.spoperation)
     c:RegisterEffect(e2)
 
-    -- Other effects (hand activation, add from deck to hand)
-    -- ... Existing effect definitions remain unchanged ...
+    -- Hand activation condition
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+    e3:SetCondition(s.handcon)
+    c:RegisterEffect(e3)
+
+    -- Graveyard effect to add Spell/Trap
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_TO_GRAVE)
+    e4:SetProperty(EFFECT_FLAG_DELAY)
+    e4:SetCondition(s.thcon)
+    e4:SetTarget(s.thtg)
+    e4:SetOperation(s.thop)
+    c:RegisterEffect(e4)
+end
+
+-- Condition to check if it's appropriate to activate the effect
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==tp or Duel.GetTurnPlayer()~=tp
 end
 
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,c) end
-    local tg=Duel.SelectMatchingCard(tp,Card.IsDiscardable,tp,LOCATION_HAND,0,1,99,c)
-    local ct=Duel.SendtoGrave(tg,REASON_COST+REASON_DISCARD)
-    e:SetLabel(ct)  -- Store the count of cards discarded to use in the target and operation
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+    local ct=Duel.DiscardHand(tp,Card.IsDiscardable,1,99,REASON_COST)
+    e:SetLabel(ct)
 end
 
 function s.sptarget(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
-        return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,e:GetLabel())
+        local ct=e:GetLabel()
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and ct>0 and
+               Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,ct,e,tp)
     end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 
 function s.spoperation(e,tp,eg,ep,ev,re,r,rp)
-    local lv=e:GetLabel()
+    local ct=e:GetLabel()
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,lv)
-    if #g>0 then
-        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,ct,e,tp)
+    if #sg>0 then
+        Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
     end
 end
 
-function s.spfilter(c,e,tp,lv)
-    return c:IsSetCard(0xb) and c:IsLevel(lv) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfilter(c,level,e,tp)
+    return c:IsSetCard(0xb) and c:IsLevel(level) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.handcon(e)
+    return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0xb),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+    return not e:GetHandler():IsReason(REASON_RETURN)
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thfilter(c)
+    return c:IsSetCard(0xb) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 end
