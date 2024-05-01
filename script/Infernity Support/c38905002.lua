a01a6b6e0fbbeb31bfe8c9375bc3691c28783fd2
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-01 20:22:59 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -4,14 +4,13 @@ function s.initial_effect(c)
     --Always treated as an "Infernity" card
     c:SetUniqueOnField(1,0,id)
 
-    --Discard and Special Summon
+    --Register activation of effect
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_SZONE)
-    e1:SetCountLimit(1)
+    e1:SetCountLimit(1, id)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
@@ -19,21 +18,17 @@ end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
-        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-        and Duel.IsPlayerCanDiscardHand(tp)
+        local ct=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and ct>0 
+               and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
     end
     local dc=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-    local d=Duel.DiscardHand(tp,nil,1,dc,REASON_DISCARD+REASON_EFFECT)
-    e:SetLabel(d)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,d,tp,LOCATION_DECK)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,dc,tp,LOCATION_DECK)
 end
 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local ct=e:GetLabel()
-    if ct>0 then
-        if Duel.GetLocationCount(tp,LOCATION_MZONE)<ct then return end
+    local ct=Duel.DiscardHand(tp,nil,1,60,REASON_EFFECT+REASON_DISCARD)
+    if ct>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>=ct then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
         local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,ct,ct,nil,e,tp)
         if #g>0 then
