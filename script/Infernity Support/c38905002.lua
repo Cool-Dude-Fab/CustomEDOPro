d0a5b741af9b966c563150f0a02b2fbd17fa09da
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-02 00:22:27 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -1,81 +1,88 @@
 --Magic Mallet Reimagined
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
+    -- Activate effect to send to grave and special summon
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+    e1:SetCategory(CATEGORY_TOGRAVE + CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 
-    --Activate from hand
+    -- Effect to activate from hand
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
     e2:SetCondition(s.handcon)
     c:RegisterEffect(e2)
 
-    --Special Summon effect
+    -- Trigger effect to add from deck to hand
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,0))
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetRange(LOCATION_SZONE)
-    e3:SetCountLimit(1, id)
-    e3:SetCost(s.spcost)
-    e3:SetTarget(s.sptarget)
-    e3:SetOperation(s.spoperation)
+    e3:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCondition(s.thcon)
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.thop)
     c:RegisterEffect(e3)
-
-    --Add from deck to hand when sent to GY
-    local e4=Effect.CreateEffect(c)
-    e4:SetDescription(aux.Stringid(id,1))
-    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-    e4:SetCode(EVENT_TO_GRAVE)
-    e4:SetCondition(s.thcon)
-    e4:SetTarget(s.thtg)
-    e4:SetOperation(s.thop)
-    c:RegisterEffect(e4)
 end
 
+-- Target for sending to grave and potential summon
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then 
+    if chk==0 then
         return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,nil) and
                Duel.GetLocationCount(tp,LOCATION_MZONE)>0
     end
-    Duel.SetTargetPlayer(tp)
     Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
 end
 
+-- Activate to send to grave and potentially special summon
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-    Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(p,Card.IsAbleToGrave,p,LOCATION_HAND,0,1,63,nil)
-    if #g==0 then return end
+    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,63,nil)
     local ct=Duel.SendtoGrave(g,REASON_EFFECT)
-    if ct>0 then
-        Duel.BreakEffect()
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,ct,e,tp)
+    if ct>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+        local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
         if #sg>0 then
             Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
         end
     end
 end
 
-function s.spfilter(c,level,e,tp)
-    return c:IsSetCard(0xb) and c:IsLevel(level) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+-- Filter for Infernity monsters that can be special summoned
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0xb) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
+-- Condition to activate from hand
+function s.handcon(e)
+    return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0xb),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+
+-- Condition to trigger add from deck to hand
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return not e:GetHandler():IsReason(REASON_RETURN)
+    return e:GetHandler():IsReason(REASON_DESTROY)
 end
 
+-- Target for adding from deck to hand
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-   
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+-- Filter for Infernity spells/traps that can be added to hand
+function s.thfilter(c)
+    return c:IsSetCard(0xb) and c:IsType(TYPE_SPELL + TYPE_TRAP) and c:IsAbleToHand()
+end
+
+-- Operation to add from deck to hand
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
