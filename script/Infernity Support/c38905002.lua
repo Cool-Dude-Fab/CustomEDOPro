0f7300d7d8c1acd8fba974c188c59c342b152475
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-02 00:03:08 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -1,33 +1,35 @@
---Infernity Resurgence
+--Magic Mallet Reimagined
 local s,id=GetID()
 function s.initial_effect(c)
-    --Continuous Trap
-    c:SetUniqueOnField(1,0,id)
-
     --Activate
     local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 
-    --Special Summon "Infernity" monster from Deck based on number of cards sent to GY
+    --Add from deck to hand
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_SZONE)
-    e2:SetCountLimit(1, id)
-    e2:SetTarget(s.target)
-    e2:SetOperation(s.activate)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetCondition(s.thcon)
+    e2:SetTarget(s.thtg)
+    e2:SetOperation(s.thop)
     c:RegisterEffect(e2)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then 
-        return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,nil) and
-               Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and
-               Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,1,e,tp) -- Check at least one level 1 monster exists
+        return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,e:GetHandler()) and
+               Duel.GetLocationCount(tp,LOCATION_MZONE)>0
     end
+    Duel.SetTargetPlayer(tp)
     Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
 end
 
@@ -39,7 +41,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local ct=Duel.SendtoGrave(g,REASON_EFFECT)
     if ct>0 then
         Duel.BreakEffect()
-        -- Special summon 1 "Infernity" monster whose level equals the number of cards sent to the GY
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
         local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,ct,e,tp)
         if #sg>0 then
@@ -50,4 +51,26 @@ end
 
 function s.spfilter(c,level,e,tp)
     return c:IsSetCard(0xb) and c:IsLevel(level) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+    return not e:GetHandler():IsReason(REASON_RETURN)
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thfilter(c)
+    return c:IsSetCard(0xb) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end
