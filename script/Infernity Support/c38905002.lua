ba895b7541e9dba09504f27b14ec7c42da5d2787
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-01 19:26:53 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -3,9 +3,6 @@ local s,id=GetID()
 function s.initial_effect(c)
     --Always treated as an “Infernity” card
     c:SetUniqueOnField(1,0,id)
-    c:EnableReviveLimit()
-    aux.AddContinuousTrapProcedure(c)
-
     --Activate from hand
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
