881b6138882e29ddf089cd24f586e3b90ee105e8
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-01 22:08:51 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -1,35 +1,37 @@
---Perpetual Exchange
+--Phantom Hand (Modified)
 local s,id=GetID()
 function s.initial_effect(c)
-    --Always treated as an "Exchange" card
-    c:SetUniqueOnField(1,0,id)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
 
-    --Activate: Return and Draw
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_SZONE)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetCountLimit(1, id)  -- "Once per turn" limit for this specific effect.
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.activate)
-    c:RegisterEffect(e1)
+	--Discard
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.discardcon)
+	e2:SetTarget(s.discardtg)
+	e2:SetOperation(s.discardop)
+	c:RegisterEffect(e2)
 end
 
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsPlayerCanDraw(tp) and 
-                     Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,nil) end
-    Duel.SetTargetPlayer(tp)
-    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
+function s.discardcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0xb),tp,LOCATION_MZONE,0,1,nil)
 end
 
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-    Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
-    local g=Duel.SelectMatchingCard(p,Card.IsAbleToDeck,p,LOCATION_HAND,0,1,63,nil)
-    if #g==0 then return end
-    Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-    Duel.ShuffleDeck(p)
-    Duel.BreakEffect()
-    Duel.Draw(p,#g,REASON_EFFECT)
+function s.discardtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_DISCARD,nil,1,tp,LOCATION_HAND)
+end
+
+function s.discardop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsDiscardable,tp,LOCATION_HAND,0,nil)
+	if #g>0 then
+		Duel.DiscardHand(tp,aux.TRUE,1,#g,REASON_EFFECT+REASON_DISCARD)
+	end
 end
