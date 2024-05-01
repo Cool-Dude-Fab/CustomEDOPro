bc9f05c1bc63b8d39e9d9cdb3425f31470f2e804
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-01 23:29:43 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -1,43 +1,59 @@
---Phantom Hand (Modified)
+--Infernity Resurgence
 local s,id=GetID()
 function s.initial_effect(c)
+    --Continuous Trap Card
+    c:SetUniqueOnField(1,0,id)
+
     --Activate
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e1)
 
-    --Discard
+    --Special Summon "Infernity" monsters
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,0))
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_SZONE)
-    e2:SetCountLimit(1)
-    e2:SetCondition(s.discardcon)
-    e2:SetTarget(s.discardtg)
-    e2:SetOperation(s.discardop)
+    e2:SetCountLimit(1, id)
+    e2:SetCost(s.spcost)
+    e2:SetTarget(s.sptarget)
+    e2:SetOperation(s.spoperation)
     c:RegisterEffect(e2)
 end
 
-function s.discardcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0xb),tp,LOCATION_MZONE,0,1,nil)
+function s.costfilter(c)
+    return c:IsDiscardable() and c:IsType(TYPE_MONSTER)
 end
 
-function s.discardtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+    local rt=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_DECK,0,nil,0xb)
+    local disc=Duel.DiscardHand(tp, s.costfilter, 1, rt, REASON_COST+REASON_DISCARD)
+    e:SetLabel(disc)
+end
+
+function s.sptarget(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
-        return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+        local ct=e:GetLabel()
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+           and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,ct,nil,e,tp)
     end
-    local dc=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
-    Duel.SetOperationInfo(0,CATEGORY_DISCARD,nil,dc,tp,LOCATION_HAND)
+    local ct=e:GetLabel()
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ct,tp,LOCATION_DECK)
 end
 
-function s.discardop(e,tp,eg,ep,ev,re,r,rp)
-    local dc=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
-    if dc>0 then
-        Duel.DiscardHand(tp,nil,1,dc,REASON_EFFECT+REASON_DISCARD)
-    else
-        -- Optionally, you can add a hint message here if no cards are available to discard
-        Duel.Hint(HINT_MESSAGE,tp,HINTMSG_DISCARD)
+function s.spoperation(e,tp,eg,ep,ev,re,r,rp)
+    local ct=e:GetLabel()
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<ct then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,ct,ct,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
+end
+
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0xb) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
