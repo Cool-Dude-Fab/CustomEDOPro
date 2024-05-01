4b06fb28df3d30bde26b6133ddc590614d51ecd0
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-02 00:17:06 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -43,9 +43,37 @@ function s.initial_effect(c)
     c:RegisterEffect(e4)
 end
 
--- Other functions unchanged
-
-function s.handcon(e)
-    return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0xb),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then 
+        return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,e:GetHandler()) and
+               Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+    end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
 end
 
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+    Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TOGRAVE)
+    local g=Duel.SelectMatchingCard(p,Card.IsAbleToGrave,p,LOCATION_HAND,0,1,63,nil)
+    if #g==0 then return end
+    local ct=Duel.SendtoGrave(g,REASON_EFFECT)
+    if ct>0 then
+        Duel.BreakEffect()
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,ct,e,tp)
+        if #sg>0 then
+            Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
+end
+
+function s.spfilter(c,level,e,tp)
+    return c:IsSetCard(0xb) and c:IsLevel(level) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+    return not e:GetHandler():IsReason(REASON_RETURN)
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,
