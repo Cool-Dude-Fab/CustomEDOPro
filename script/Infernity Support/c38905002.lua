be171534c6cc760477f7921978febb7569233d66
Fabs <31155026+Cool-Dude-Fab@users.noreply.github.com>
2024-05-01 19:10:41 +0100

    Add files via upload

---
+++ script/Infernity Support/c38905002.lua
@@ -0,0 +1,90 @@
+--Enforcers, Together Forever!
+local s,id=GetID()
+function s.initial_effect(c)
+    --Always treated as an “Infernity” card
+    c:SetUniqueOnField(1,0,id)
+    c:EnableReviveLimit()
+    aux.AddContinuousTrapProcedure(c)
+
+    --Activate from hand
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+    e1:SetCondition(s.handcon)
+    c:RegisterEffect(e1)
+
+    --Discard and Special Summon
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DISCARD)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCountLimit(1)
+    e2:SetCondition(s.spcon)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
+    c:RegisterEffect(e2)
+
+    --Add 1 “Infernity” Spell/Trap from your Deck
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetCondition(s.thcon)
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.thop)
+    c:RegisterEffect(e3)
+end
+
+function s.handcon(e)
+    return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0xb),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0xb),tp,LOCATION_MZONE,0,1,nil)
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
+    Duel.SetOperationInfo(0,CATEGORY_DISCARD,nil,0,tp,1)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_DECK)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local ct=Duel.DiscardHand(tp,aux.TRUE,1,60,REASON_DISCARD+REASON_EFFECT)
+    if ct>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,ct)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
+end
+
+function s.spfilter(c,lv)
+    return c:IsSetCard(0xb) and c:IsLevel(lv) and c:IsCanBeSpecialSummoned()
+end
+
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+    return rp~=tp and e:GetHandler():IsPreviousLocation(LOCATION_SZONE)
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thfilter(c)
+    return c:IsSetCard(0xb) and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsCode(id) and c:IsAbleToHand()
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
