f9ee9edd8ffe7b2cda19736807be3be0423aa9c0
Rocco <60403275+EpicRocco11@users.noreply.github.com>
2024-05-02 01:23:04 +0100

    Update c38905002.lua

--- script/Infernity Support/c38905002.lua
+++ script/Infernity Support/c38905002.lua
@@ -17,18 +17,6 @@ function s.initial_effect(c)
     e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
     e2:SetCondition(s.handcon)
     c:RegisterEffect(e2)
-
-    --Add from deck to hand
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-    e3:SetCode(EVENT_TO_GRAVE)
-    e3:SetCondition(s.thcon)
-    e3:SetTarget(s.thtg)
-    e3:SetOperation(s.thop)
-    c:RegisterEffect(e3)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -58,28 +46,6 @@ end
 
 function s.spfilter(c,level,e,tp)
     return c:IsSetCard(0xb) and c:IsLevel(level) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return not e:GetHandler():IsReason(REASON_RETURN)
-end
-
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-
-function s.thfilter(c)
-    return c:IsSetCard(0xb) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
-end
-
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-    if #g>0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
-    end
 end
 
 function s.handcon(e)
